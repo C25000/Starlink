72b49d9c31615986f6da317eef300ac4b7ae9723
zchang <csuchangzheng@gmail.com>
2021-09-01 22:35:45 -0500

    add worldmap js

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SAT_API_KEY = "WRMWZL-CXN9BG-4D83QE-4RQJ";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
